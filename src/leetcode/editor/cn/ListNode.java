b517d98a6bf7a722d776f01a73310f72a7899611
damianzhenxiaozhi <tomfan@163.com>
2021-05-25 23:34:02 +0800

    week2

---
+++ src/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,23 @@
+package leetcode.editor.cn;
+
+/**
+ * @author fanzhen
+ * @version v1
+ * @date 2021/5/25
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
