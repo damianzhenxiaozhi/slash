9388c730632ab80841855017c9eb21435d0d8a98
fanzhen <fanzhen@owinfo.net>
2021-06-22 18:04:11 +0800

    week6

--- src/leetcode/editor/cn/ErChaSouSuoShuYuShuangXiangLianBiaoLcof.java
+++ src/leetcode/editor/cn/ErChaSouSuoShuYuShuangXiangLianBiaoLcof.java
@@ -95,5 +95,23 @@ class Node {
         }
 
     }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
 //leetcode submit region end(Prohibit modification and deletion)
 }
