08c2646ca34803e2e189bcd33a18569fad3bdcbb
fanzhen <fanzhen@owinfo.net>
2021-08-17 18:08:50 +0800

    week14

--- src/leetcode/editor/cn/MaximumSubarray.java
+++ src/leetcode/editor/cn/MaximumSubarray.java
@@ -59,13 +59,28 @@ package leetcode.editor.cn;
 public class MaximumSubarray {
     public static void main(String[] args) {
         Solution s = new MaximumSubarray().new Solution();
-        int[] nums = {1};
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(s.maxSubArray(nums));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxSubArray(int[] nums) {
+            int max = Integer.MIN_VALUE;
+            int sum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (sum < 0) {
+                    sum = 0;
+                }
+                sum += nums[i];
+                max = Math.max(max, sum);
+            }
+
+            return max;
+        }
+
+        public int maxSubArray3(int[] nums) {
             int n = nums.length;
             int[] sums = new int[n];
             int sum = 0;
@@ -81,7 +96,12 @@ public class MaximumSubarray {
                 maxSums[i] = maxSum;
             }
 
+            int result = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                result = Math.max(result, maxSums[i] - sums[i] + nums[i]);
+            }
 
+            return result;
         }
 
         public int maxSubArray2(int[] nums) {
